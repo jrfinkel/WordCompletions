7d14ba4bc39ddd2d9df67f1553287888c59b97cd
Ted Scharff <e.b.scharff@nasa.gov>
2011-06-26 23:48:31 -0700

    Adds - android subtree - SDK-generated boilerplate - NgramLatinIME stub - Manifest

---
+++ android/src/com/android/inputmethod/ngram/NgramKeyboardActivity.java
@@ -0,0 +1,15 @@
+package com.android.inputmethod.ngram;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NgramKeyboardActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ android/src/com/android/inputmethod/ngram/NgramLatinIME.java
@@ -0,0 +1,8 @@
+package com.android.inputmetod.ngram;
+
+import android.inputmethodservice.Keyboard;
+import com.android.inputmethod.latin.LatinIME;
+
+public class NgramLatinIME extends LatinIME {
+
+}
